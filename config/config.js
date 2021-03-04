3ef359af2cd708d6700b646deaa2b117357e40e3
Jonathan <62848142+jtejedera@users.noreply.github.com>
2021-03-04 12:59:10 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 export default {
     'port': process.env.NODE_PORT || 3000,
     'API_URL': 'https://dare-nodejs-assessment.herokuapp.com/api/',
-    'API_CLIENT': 'dare',
-    'API_PASSWORD': 's3cr3t'
+    'API_CLIENT': '',
+    'API_PASSWORD': ''
   };
   
